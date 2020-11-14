95b1c9c1188c8be49e854a1bbdb722021e38d3ca
Martin <martin.lippl@pueo.de>
2020-11-15 00:27:14 +0100

    Better indentation

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -10,23 +10,22 @@ terraform {
 
 
 provider "aws" {
-    profile = "default"
-    region = "eu-central-1"
+  profile = "default"
+  region  = "eu-central-1"
 }
 
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "udacityT2" {
-    count = "4"
-    ami = "ami-00a205cb8e06c3c4e"
-    instance_type = "t2.micro"
+  count         = "4"
+  ami           = "ami-00a205cb8e06c3c4e"
+  instance_type = "t2.micro"
 }
-
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 resource "aws_instance" "udacityM4" {
-    count = "2"
-    ami = "ami-00a205cb8e06c3c4e"
-    instance_type = "m4.large"
+  count         = "2"
+  ami           = "ami-00a205cb8e06c3c4e"
+  instance_type = "m4.large"
 }
