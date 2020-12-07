14eb1e6ce16b2b47c78eb584f81413f48b7adc5f
Martin Lippl <martin.lippl@pueo.de>
2020-12-07 17:40:43 +0100

    * Some minor modifications in main-module (e.g. added automatic zipping,   some renaming) * New version of zip-file (not sure if we should exclude this in the   future)

--- task5/Exercise_2/main.tf
+++ task5/Exercise_2/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.00"
+      version = ">= 3.00"
     }
   }
 }
@@ -12,10 +12,22 @@ provider "aws" {
   region  = var.region
 }
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "greet_lambda.py"
+  output_path = "greet_lambda.zip"
+}
+
 resource "aws_lambda_function" "my_lambda_fct" {
   filename      = "greet_lambda.zip"
   function_name = "greet_lambda"
   role          = aws_iam_role.lambda_exec.arn
-  handler       = "greet_lambda.handler"
+  handler       = "greet_lambda.lambda_handler"
   runtime       = "python3.6"
+  environment {
+    variables = {
+      greeting = "Live long and prosper"
+    }
+  }
 }
+
